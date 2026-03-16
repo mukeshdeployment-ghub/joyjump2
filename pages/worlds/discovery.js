eb7dc76d4daadc08d684cd0bc0c90e0965784943
mukeshdeployment-ghub <mukeshdeployment@gmail.com>
2026-03-16 13:01:28 +0530

    Delete pages directory

--- pages/worlds/discovery.js
+++
@@ -1,5 +0,0 @@
-import WorldPage from '../../components/world/WorldPage'
-
-export default function DiscoverySpace() {
-  return <WorldPage worldId="discovery" curriculumData={null} />
-}
--- pages/worlds/index.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
-export default function WorldsIndex() {
-  const router = useRouter()
-  useEffect(() => { router.replace('/') }, [])
-  return null
-}
